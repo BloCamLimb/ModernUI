d9d5007f2d6d10d8f3e41592d431b8772c051c3d
BloCamLimb <pocamelards@gmail.com>
2020-07-30 23:49:32 +0800

    2.0.2 pre38

--- src/main/java/icyllis/modernui/system/mixin/MixinMainWindow.java
+++ src/main/java/icyllis/modernui/system/mixin/MixinMainWindow.java
@@ -18,7 +18,7 @@
 
 package icyllis.modernui.system.mixin;
 
-import icyllis.modernui.system.MixinMethods;
+import icyllis.modernui.system.MixinTools;
 import net.minecraft.client.MainWindow;
 import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
@@ -29,11 +29,11 @@ public class MixinMainWindow {
 
     /**
      * @author BloCamLimb
-     * @reason Make GUI scale suitable, and not limited to even numbers when forceUnicode = true
+     * @reason Make GUI scale more suitable, and not limited to even numbers when forceUnicode = true
      */
     @Overwrite
     public int calcGuiScale(int guiScaleIn, boolean forceUnicode) {
-        int r = MixinMethods.calcGuiScales((MainWindow) (Object) this);
+        int r = MixinTools.calcGuiScales((MainWindow) (Object) this);
         return guiScaleIn > 0 ? MathHelper.clamp(guiScaleIn, r >> 8 & 0xf, r & 0xf) : r >> 4 & 0xf;
     }
 }
