8caab0f0217729d055a4e2cbf9bd876b09bfc840
Icyllis Milica <pocamelards@gmail.com>
2021-02-03 19:42:40 +0800

    Fix maven upload

--- mod-common/src/main/java/icyllis/modernui/util/Pool.java
+++ mod-common/src/main/java/icyllis/modernui/util/Pool.java
@@ -16,7 +16,30 @@
  * License along with Modern UI. If not, see <https://www.gnu.org/licenses/>.
  */
 
-package icyllis.modernui.text;
+package icyllis.modernui.util;
 
-public class MeasuredParagraph {
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * Interface for managing a pool of objects.
+ *
+ * @param <T> The pooled type.
+ */
+public interface Pool<T> {
+
+    /**
+     * @return An instance from the pool if such, null otherwise.
+     */
+    @Nullable
+    T acquire();
+
+    /**
+     * Release an instance to the pool.
+     *
+     * @param instance The instance to release.
+     * @return Whether the instance was put in the pool.
+     * @throws IllegalStateException If the instance is already in the pool.
+     */
+    boolean release(@Nonnull T instance);
 }
