b08bc82a8ecd543b1faaea7ccb4e312c37f2d276
Icyllis Milica <pocamelards@gmail.com>
2021-10-27 23:15:43 +0800

    Add Replacement

--- core/src/main/java/icyllis/modernui/text/style/ReplacementSpan.java
+++ core/src/main/java/icyllis/modernui/text/style/ReplacementSpan.java
@@ -18,6 +18,55 @@
 
 package icyllis.modernui.text.style;
 
-//TODO
+import icyllis.modernui.graphics.Canvas;
+import icyllis.modernui.text.FontMetricsInt;
+import icyllis.modernui.text.FontPaint;
+import icyllis.modernui.text.TextPaint;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 public abstract class ReplacementSpan extends MetricAffectingSpan {
+
+    /**
+     * This method does nothing, since ReplacementSpans are measured
+     * explicitly instead of affecting Paint properties.
+     */
+    @Override
+    public final void updateMeasureState(@Nonnull FontPaint paint) {
+    }
+
+    /**
+     * Returns the width of the span. Extending classes can set the height of the span by updating
+     * attributes of {@link FontMetricsInt}. If the span covers the whole
+     * text, and the height is not set,
+     * {@link #draw(Canvas, CharSequence, int, int, float, int, int, int, TextPaint)} will not be
+     * called for the span.
+     *
+     * @param paint Paint instance.
+     * @param text  Current text.
+     * @param start Start character index for span.
+     * @param end   End character index for span.
+     * @param fm    Font metrics, can be null.
+     * @return Width of the span.
+     */
+    public abstract int getSize(@Nonnull FontPaint paint, CharSequence text,
+                                int start, int end, @Nullable FontMetricsInt fm);
+
+    /**
+     * Draws the span into the canvas.
+     *
+     * @param canvas Canvas into which the span should be rendered.
+     * @param text   Current text.
+     * @param start  Start character index for span.
+     * @param end    End character index for span.
+     * @param x      Edge of the replacement closest to the leading margin.
+     * @param top    Top of the line.
+     * @param y      Baseline.
+     * @param bottom Bottom of the line.
+     * @param paint  Paint instance.
+     */
+    public abstract void draw(@Nonnull Canvas canvas, CharSequence text,
+                              int start, int end, float x, int top, int y, int bottom,
+                              @Nonnull TextPaint paint);
 }
