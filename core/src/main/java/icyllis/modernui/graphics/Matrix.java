0a12b357be38418c67c51f252b29e0b918f23b15
Icyllis Milica <pocamelards@gmail.com>
2023-09-28 17:42:00 +0800

    Update Matrix

--- core/src/main/java/icyllis/modernui/graphics/Matrix.java
+++ core/src/main/java/icyllis/modernui/graphics/Matrix.java
@@ -42,26 +42,26 @@ public class Matrix extends icyllis.arc3d.core.Matrix {
      * @param r the rectangle to transform
      */
     public void mapRect(@Nonnull RectF r) {
-        float x1 = m11 * r.left + m21 * r.top + m31;
-        float y1 = m12 * r.left + m22 * r.top + m32;
-        float x2 = m11 * r.right + m21 * r.top + m31;
-        float y2 = m12 * r.right + m22 * r.top + m32;
-        float x3 = m11 * r.left + m21 * r.bottom + m31;
-        float y3 = m12 * r.left + m22 * r.bottom + m32;
-        float x4 = m11 * r.right + m21 * r.bottom + m31;
-        float y4 = m12 * r.right + m22 * r.bottom + m32;
+        float x1 = m11 * r.left + m21 * r.top + m41;
+        float y1 = m12 * r.left + m22 * r.top + m42;
+        float x2 = m11 * r.right + m21 * r.top + m41;
+        float y2 = m12 * r.right + m22 * r.top + m42;
+        float x3 = m11 * r.left + m21 * r.bottom + m41;
+        float y3 = m12 * r.left + m22 * r.bottom + m42;
+        float x4 = m11 * r.right + m21 * r.bottom + m41;
+        float y4 = m12 * r.right + m22 * r.bottom + m42;
         if (hasPerspective()) {
             // project
-            float w = 1.0f / (m13 * r.left + m23 * r.top + m33);
+            float w = 1.0f / (m14 * r.left + m24 * r.top + m44);
             x1 *= w;
             y1 *= w;
-            w = 1.0f / (m13 * r.right + m23 * r.top + m33);
+            w = 1.0f / (m14 * r.right + m24 * r.top + m44);
             x2 *= w;
             y2 *= w;
-            w = 1.0f / (m13 * r.left + m23 * r.bottom + m33);
+            w = 1.0f / (m14 * r.left + m24 * r.bottom + m44);
             x3 *= w;
             y3 *= w;
-            w = 1.0f / (m13 * r.right + m23 * r.bottom + m33);
+            w = 1.0f / (m14 * r.right + m24 * r.bottom + m44);
             x4 *= w;
             y4 *= w;
         }
@@ -95,26 +95,26 @@ public class Matrix extends icyllis.arc3d.core.Matrix {
      * @param out the round values
      */
     public void mapRect(float l, float t, float r, float b, @Nonnull Rect out) {
-        float x1 = m11 * l + m21 * t + m31;
-        float y1 = m12 * l + m22 * t + m32;
-        float x2 = m11 * r + m21 * t + m31;
-        float y2 = m12 * r + m22 * t + m32;
-        float x3 = m11 * l + m21 * b + m31;
-        float y3 = m12 * l + m22 * b + m32;
-        float x4 = m11 * r + m21 * b + m31;
-        float y4 = m12 * r + m22 * b + m32;
+        float x1 = m11 * l + m21 * t + m41;
+        float y1 = m12 * l + m22 * t + m42;
+        float x2 = m11 * r + m21 * t + m41;
+        float y2 = m12 * r + m22 * t + m42;
+        float x3 = m11 * l + m21 * b + m41;
+        float y3 = m12 * l + m22 * b + m42;
+        float x4 = m11 * r + m21 * b + m41;
+        float y4 = m12 * r + m22 * b + m42;
         if (hasPerspective()) {
             // project
-            float w = 1.0f / (m13 * l + m23 * t + m33);
+            float w = 1.0f / (m14 * l + m24 * t + m44);
             x1 *= w;
             y1 *= w;
-            w = 1.0f / (m13 * r + m23 * t + m33);
+            w = 1.0f / (m14 * r + m24 * t + m44);
             x2 *= w;
             y2 *= w;
-            w = 1.0f / (m13 * l + m23 * b + m33);
+            w = 1.0f / (m14 * l + m24 * b + m44);
             x3 *= w;
             y3 *= w;
-            w = 1.0f / (m13 * r + m23 * b + m33);
+            w = 1.0f / (m14 * r + m24 * b + m44);
             x4 *= w;
             y4 *= w;
         }
@@ -148,26 +148,26 @@ public class Matrix extends icyllis.arc3d.core.Matrix {
      * @param out the round out values
      */
     public void mapRectOut(float l, float t, float r, float b, @Nonnull Rect out) {
-        float x1 = m11 * l + m21 * t + m31;
-        float y1 = m12 * l + m22 * t + m32;
-        float x2 = m11 * r + m21 * t + m31;
-        float y2 = m12 * r + m22 * t + m32;
-        float x3 = m11 * l + m21 * b + m31;
-        float y3 = m12 * l + m22 * b + m32;
-        float x4 = m11 * r + m21 * b + m31;
-        float y4 = m12 * r + m22 * b + m32;
+        float x1 = m11 * l + m21 * t + m41;
+        float y1 = m12 * l + m22 * t + m42;
+        float x2 = m11 * r + m21 * t + m41;
+        float y2 = m12 * r + m22 * t + m42;
+        float x3 = m11 * l + m21 * b + m41;
+        float y3 = m12 * l + m22 * b + m42;
+        float x4 = m11 * r + m21 * b + m41;
+        float y4 = m12 * r + m22 * b + m42;
         if (hasPerspective()) {
             // project
-            float w = 1.0f / (m13 * l + m23 * t + m33);
+            float w = 1.0f / (m14 * l + m24 * t + m44);
             x1 *= w;
             y1 *= w;
-            w = 1.0f / (m13 * r + m23 * t + m33);
+            w = 1.0f / (m14 * r + m24 * t + m44);
             x2 *= w;
             y2 *= w;
-            w = 1.0f / (m13 * l + m23 * b + m33);
+            w = 1.0f / (m14 * l + m24 * b + m44);
             x3 *= w;
             y3 *= w;
-            w = 1.0f / (m13 * r + m23 * b + m33);
+            w = 1.0f / (m14 * r + m24 * b + m44);
             x4 *= w;
             y4 *= w;
         }
@@ -183,14 +183,14 @@ public class Matrix extends icyllis.arc3d.core.Matrix {
      * @param p the point to transform
      */
     public void mapPoint(@Nonnull PointF p) {
-        if (getType() <= Affine_Mask) {
-            p.set(m11 * p.x + m21 * p.y + m31,
-                    m12 * p.x + m22 * p.y + m32);
+        if (getType() <= kAffine_Mask) {
+            p.set(m11 * p.x + m21 * p.y + m41,
+                    m12 * p.x + m22 * p.y + m42);
         } else {
             // project
-            final float x = m11 * p.x + m21 * p.y + m31;
-            final float y = m12 * p.x + m22 * p.y + m32;
-            float w = 1.0f / (m13 * p.x + m23 * p.y + m33);
+            final float x = m11 * p.x + m21 * p.y + m41;
+            final float y = m12 * p.x + m22 * p.y + m42;
+            float w = 1.0f / (m14 * p.x + m24 * p.y + m44);
             p.x = x * w;
             p.y = y * w;
         }
@@ -202,15 +202,15 @@ public class Matrix extends icyllis.arc3d.core.Matrix {
      * @param p the point to transform
      */
     public void mapPoint(@Nonnull float[] p) {
-        if (getType() <= Affine_Mask) {
-            final float x = m11 * p[0] + m21 * p[1] + m31;
-            final float y = m12 * p[0] + m22 * p[1] + m32;
+        if (getType() <= kAffine_Mask) {
+            final float x = m11 * p[0] + m21 * p[1] + m41;
+            final float y = m12 * p[0] + m22 * p[1] + m42;
             p[0] = x;
             p[1] = y;
         } else {
-            final float x = m11 * p[0] + m21 * p[1] + m31;
-            final float y = m12 * p[0] + m22 * p[1] + m32;
-            float w = 1.0f / (m13 * p[0] + m23 * p[1] + m33);
+            final float x = m11 * p[0] + m21 * p[1] + m41;
+            final float y = m12 * p[0] + m22 * p[1] + m42;
+            float w = 1.0f / (m14 * p[0] + m24 * p[1] + m44);
             p[0] = x * w;
             p[1] = y * w;
         }
