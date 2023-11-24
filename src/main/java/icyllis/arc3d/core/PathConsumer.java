27562d97dfb4f9eeb60089615bc7091e445596ac
Icyllis Milica <pocamelards@gmail.com>
2023-11-24 23:02:27 +0800

    Add PathConsumer

---
+++ src/main/java/icyllis/arc3d/core/PathConsumer.java
@@ -0,0 +1,87 @@
+/*
+ * Modern UI.
+ * Copyright (C) 2019-2023 BloCamLimb. All rights reserved.
+ *
+ * Modern UI is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Modern UI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Modern UI. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package icyllis.arc3d.core;
+
+/**
+ * Accepts {@link Path} elements.
+ */
+public interface PathConsumer {
+
+    /**
+     * Accepts a point to the path consumer by moving to the specified
+     * point {@code (x,y)}.
+     *
+     * @param x the specified X coordinate
+     * @param y the specified Y coordinate
+     */
+    void moveTo(float x, float y);
+
+    /**
+     * Accepts a point to the path by drawing a straight line from the
+     * current point to the new specified point {@code (x,y)}.
+     *
+     * @param x the specified X coordinate
+     * @param y the specified Y coordinate
+     */
+    void lineTo(float x, float y);
+
+    /**
+     * Accepts a curved segment, defined by two new points, to the path by
+     * drawing a quadratic Bézier curve that intersects both the current
+     * point and the specified point {@code (x2,y2)}, using the specified
+     * point {@code (x1,y1)} as a quadratic control point.
+     *
+     * @param x1 the X coordinate of the quadratic control point
+     * @param y1 the Y coordinate of the quadratic control point
+     * @param x2 the X coordinate of the final end point
+     * @param y2 the Y coordinate of the final end point
+     */
+    void quadTo(float x1, float y1,
+                float x2, float y2);
+
+    /**
+     * Accepts a curved segment, defined by three new points, to the path by
+     * drawing a cubic Bézier curve that intersects both the current
+     * point and the specified point {@code (x3,y3)}, using the specified
+     * points {@code (x1,y1)} and {@code (x2,y2)} as cubic control points.
+     *
+     * @param x1 the X coordinate of the first cubic control point
+     * @param y1 the Y coordinate of the first cubic control point
+     * @param x2 the X coordinate of the second cubic control point
+     * @param y2 the Y coordinate of the second cubic control point
+     * @param x3 the X coordinate of the final end point
+     * @param y3 the Y coordinate of the final end point
+     */
+    void cubicTo(float x1, float y1,
+                 float x2, float y2,
+                 float x3, float y3);
+
+    /**
+     * Closes the current sub-path by drawing a straight line back to
+     * the point of the last {@link #moveTo}.  If the path is already
+     * closed then this method has no effect.
+     */
+    void closePath();
+
+    /**
+     * Called after the last segment of the last sub-path when the
+     * iteration of the path segments is completely done.
+     */
+    void pathDone();
+}
--- src/test/java/icyllis/arc3d/test/TestTessellate.java
+++ src/test/java/icyllis/arc3d/test/TestTessellate.java
@@ -24,6 +24,7 @@ import icyllis.arc3d.engine.PathUtils;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.geom.Line2D;
+import java.awt.geom.Path2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -31,25 +32,27 @@ import java.io.IOException;
 public class TestTessellate {
 
     public static void main(String[] args) throws IOException {
-        float x0 = 220;
-        float y0 = 120;
-        float x1 = 320;
-        float y1 = 140;
-        float x2 = 220;
-        float y2 = 250;
-        float x3 = 270;
-        float y3 = 460;
-        if (true) {
-            x0 = (int) (Math.random() * 512);
-            y0 = (int) (Math.random() * 512);
-            x1 = (int) (Math.random() * 512);
-            y1 = (int) (Math.random() * 512);
-            x2 = (int) (Math.random() * 512);
-            y2 = (int) (Math.random() * 512);
-            x3 = (int) (Math.random() * 512);
-            y3 = (int) (Math.random() * 512);
-        }
-        float tol = PathUtils.DEFAULT_TOLERANCE;
+        float x0 = 163;
+        float y0 = 119;
+        float x1 = 61;
+        float y1 = 5;
+        float x2 = 108;
+        float y2 = 194;
+        float x3 = 42;
+        float y3 = 94;
+        /*if (true) {
+            x0 = (int) (Math.random() * 256);
+            y0 = (int) (Math.random() * 256);
+            x1 = (int) (Math.random() * 256);
+            y1 = (int) (Math.random() * 256);
+            x2 = (int) (Math.random() * 256);
+            y2 = (int) (Math.random() * 256);
+            x3 = (int) (Math.random() * 256);
+            y3 = (int) (Math.random() * 256);
+        }*/
+        System.out.printf("%.0f %.0f %.0f %.0f %.0f %.0f %.0f %.0f\n",
+                x0, y0, x1, y1, x2, y2, x3, y3);
+        float tol = 0.05f;
         int numPoints = PathUtils.countCubicPoints(
                 x0, y0, x1, y1, x2, y2, x3, y3,
                 tol
@@ -64,19 +67,30 @@ public class TestTessellate {
         );
         System.out.println("Number of Points from Tessellation " + (actualNumCoords >> 1));
 
-        var img = new BufferedImage(512, 512, BufferedImage.TYPE_BYTE_GRAY);
-        var g2d = img.createGraphics();
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        var img1 = new BufferedImage(256, 256, BufferedImage.TYPE_BYTE_GRAY);
+        var img2 = new BufferedImage(256, 256, BufferedImage.TYPE_BYTE_GRAY);
+        var g2d1 = img1.createGraphics();
+        g2d1.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        var g2d2 = img2.createGraphics();
+        g2d2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         float lastX = x0;
         float lastY = y0;
+        var path = new Path2D.Float();
+        path.moveTo(x0, y0);
         for (int i = 0; i < actualNumCoords; i += 2) {
             var line = new Line2D.Float(lastX, lastY, coords[i], coords[i | 1]);
-            g2d.draw(line);
+            path.lineTo(line.x2, line.y2);
             System.out.printf("Line %d: (%f, %f) to (%f, %f)\n",
                     i >> 1, line.x1, line.y1, line.x2, line.y2);
             lastX = line.x2;
             lastY = line.y2;
         }
-        ImageIO.write(img, "png", new File("test_tessellate.png"));
+        g2d1.draw(path);
+        path.reset();
+        path.moveTo(x0, y0);
+        path.curveTo(x1, y1, x2, y2, x3, y3);
+        g2d2.draw(path);
+        ImageIO.write(img1, "png", new File("test_tessellate1.png"));
+        ImageIO.write(img2, "png", new File("test_tessellate2.png"));
     }
 }
