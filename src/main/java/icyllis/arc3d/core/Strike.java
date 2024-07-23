81134bdb7fe1270fa93128cfc3f791cdded37008
Icyllis Milica <pocamelards@gmail.com>
2024-07-23 20:06:08 +0800

    Implement Strike, StrikeCache, and ScalerContext basically

--- src/main/java/icyllis/arc3d/core/Strike.java
+++ src/main/java/icyllis/arc3d/core/Strike.java
@@ -19,5 +19,143 @@
 
 package icyllis.arc3d.core;
 
-public abstract class Strike {
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import org.jetbrains.annotations.ApiStatus;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.GuardedBy;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * The font strike holds the results from {@link ScalerContext}.
+ */
+public final class Strike {
+
+    // The following are const and need no lock protection.
+    private final StrikeDesc mStrikeDesc;
+    private final StrikeCache mStrikeCache;
+
+    private final ReentrantLock mLock = new ReentrantLock();
+
+    @GuardedBy("mLock")
+    private final Int2ObjectOpenHashMap<Glyph> mGlyphs =
+            new Int2ObjectOpenHashMap<>();
+    @GuardedBy("mLock")
+    private final ScalerContext mScalerContext;
+    @GuardedBy("mLock")
+    private long mMemoryIncrease;
+
+    // The following are guarded by mStrikeCache.mLock
+    Strike mNext;
+    Strike mPrev;
+    long mMemoryUsed;
+    boolean mRemoved;
+
+    // Use StrikeCache to obtain an instance
+    @ApiStatus.Internal
+    public Strike(@Nonnull StrikeCache strikeCache,
+                  @Nonnull StrikeDesc strikeDesc, // immutable, no copy
+                  @Nonnull ScalerContext scalerContext) {
+        mStrikeDesc = strikeDesc;
+        mStrikeCache = strikeCache;
+        mScalerContext = scalerContext;
+        // approximate bytes used
+        mMemoryUsed = 16 + 56 + 8 + 8 + 8 + 8 + 80 + 8 + 8 + 8 + 8 + 8;
+    }
+
+    public void lock() {
+        mLock.lock();
+        mMemoryIncrease = 0;
+    }
+
+    public void unlock() {
+        final long increase = mMemoryIncrease;
+        mLock.unlock();
+        if (increase > 0) {
+            // mRemoved and the cache's total memory are managed under the cache's lock. This allows
+            // them to be accessed under LRU operation.
+            mStrikeCache.mLock.lock();
+            try {
+                mMemoryUsed += increase;
+                if (!mRemoved) {
+                    mStrikeCache.mTotalMemoryUsed += increase;
+                }
+            } finally {
+                mStrikeCache.mLock.unlock();
+            }
+        }
+    }
+
+    /**
+     * Find or create a glyph for the given glyph ID, return the pointer to it.
+     * <p>
+     * Requires lock.
+     *
+     * @param glyphID typeface-specified glyph ID
+     */
+    @Nonnull
+    Glyph getGlyph(int glyphID) {
+        return digestFor(Glyph.kDirectMask, glyphID);
+    }
+
+    /**
+     * Find or create a glyph for the given glyph ID, return the pointer to it.
+     * Get or compute the digest for the given action type, ensure that
+     * {@link Glyph#actionFor(int)} is set.
+     * <p>
+     * Requires lock.
+     *
+     * @param actionType e.g. {@link Glyph#kDirectMask}
+     * @param glyphID    typeface-specified glyph ID
+     */
+    @Nonnull
+    public Glyph digestFor(int actionType, int glyphID) {
+        assert mLock.isLocked();
+        Glyph glyph = mGlyphs.get(glyphID);
+        if (glyph != null && glyph.actionFor(actionType) != Glyph.kUnset_Action) {
+            return glyph;
+        }
+
+        if (glyph == null) {
+            glyph = mScalerContext.makeGlyph(glyphID);
+            glyph.initActions();
+            mGlyphs.put(glyphID, glyph);
+            mMemoryIncrease += Glyph.kSizeOf;
+        }
+
+        glyph.setActionFor(actionType, this);
+
+        return glyph;
+    }
+
+    /**
+     * Prepare the glyph to draw an image, and return if the image exists.
+     * <p>
+     * Requires lock.
+     */
+    public boolean prepareForImage(@Nonnull Glyph glyph) {
+        assert mLock.isLocked();
+        if (glyph.setImage(mScalerContext)) {
+            mMemoryIncrease += glyph.getImageSize() + 16;
+        }
+        return glyph.getImageBase() != null;
+    }
+
+    /**
+     * Prepare the glyph to draw a path, and return if the path exists.
+     * <p>
+     * Requires lock.
+     */
+    public boolean prepareForPath(Glyph glyph) {
+        assert mLock.isLocked();
+        if (glyph.setPath(mScalerContext)) {
+            mMemoryIncrease += glyph.getPath().estimatedByteSize();
+        }
+        return glyph.getPath() != null;
+    }
+
+    // read only!!
+    public StrikeDesc getStrikeDesc() {
+        return mStrikeDesc;
+    }
 }
