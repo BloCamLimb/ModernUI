fb9456d772b78c2d2b15602008b15a9488ce89e5
Icyllis Milica <pocamelards@gmail.com>
2024-11-09 18:19:50 +0800

    Bump version number

--- core/src/main/java/icyllis/modernui/core/Core.java
+++ core/src/main/java/icyllis/modernui/core/Core.java
@@ -246,8 +246,13 @@ public final class Core {
         LOGGER.info(MARKER, "OpenGL vendor: {}", glVendor);
         LOGGER.info(MARKER, "OpenGL renderer: {}", glRenderer);
         LOGGER.info(MARKER, "OpenGL version: {}", glVersion);
-
-        LOGGER.info(MARKER, "OpenGL caps: {}", dc.getCaps());
+        if (LOGGER.isDebugEnabled(MARKER)) {
+            LOGGER.debug(MARKER, "OpenGL caps: {}", dc.getCaps());
+        } else {
+            StringBuilder b = new StringBuilder("\n");
+            ((icyllis.arc3d.opengl.GLCaps) dc.getCaps()).dump(b, false);
+            LOGGER.info(MARKER, "OpenGL caps: {}", b);
+        }
         return true;
     }
 
