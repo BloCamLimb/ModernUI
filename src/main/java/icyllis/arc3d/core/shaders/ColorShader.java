6deab1deb30ae8a4e939ff61fc556c050be139e7
Icyllis Milica <pocamelards@gmail.com>
2025-01-24 17:03:51 +0800

    Merge Color4fShader with ColorShader

--- src/main/java/icyllis/arc3d/core/shaders/ColorShader.java
+++ src/main/java/icyllis/arc3d/core/shaders/ColorShader.java
@@ -19,17 +19,47 @@
 
 package icyllis.arc3d.core.shaders;
 
+import icyllis.arc3d.core.MathUtil;
+import org.jetbrains.annotations.Contract;
+import org.jspecify.annotations.NonNull;
+
+/**
+ * A Shader that represents a single color. In general, this effect can be accomplished by just
+ * using the color field on the paint, but if an actual shader object is needed, this provides that
+ * feature.  Note: like all shaders, at draw time the paint's alpha will be respected, and is
+ * applied to the specified color.
+ */
 public final class ColorShader implements Shader {
 
-    private final int mColor;
+    // stored in non-premultiplied alpha, extended sRGB
+    private final float mR;
+    private final float mG;
+    private final float mB;
+    private final float mA;
 
+    /**
+     * Create a ColorShader wrapping the given sRGB color.
+     */
     public ColorShader(int color) {
-        mColor = color;
+        mR = ((color >> 16) & 0xff) / 255.0f;
+        mG = ((color >> 8) & 0xff) / 255.0f;
+        mB = (color & 0xff) / 255.0f;
+        mA = (color >>> 24) / 255.0f;
+    }
+
+    /**
+     * Create a ColorShader wrapping the given sRGB color.
+     */
+    public ColorShader(float r, float g, float b, float a) {
+        mR = r;
+        mG = g;
+        mB = b;
+        mA = MathUtil.pin(a, 0.0f, 1.0f);
     }
 
     @Override
     public boolean isOpaque() {
-        return (mColor >>> 24) == 255;
+        return mA == 1.0f;
     }
 
     @Override
@@ -37,7 +67,11 @@ public final class ColorShader implements Shader {
         return true;
     }
 
-    public int getColor() {
-        return mColor;
+    /**
+     * @return a copy of non-premultiplied color.
+     */
+    @Contract(value = " -> new", pure = true)
+    public float @NonNull [] getColor() {
+        return new float[]{mR, mG, mB, mA};
     }
 }
