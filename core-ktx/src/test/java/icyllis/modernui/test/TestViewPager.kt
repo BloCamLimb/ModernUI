c9ab377ffe544f4de107a959a6324ba9659111f6
Icyllis Milica <pocamelards@gmail.com>
2025-06-07 13:00:13 +0800

    Add RTL support and fix bugs for ViewPager

--- core-ktx/src/test/java/icyllis/modernui/test/TestViewPager.kt
+++ core-ktx/src/test/java/icyllis/modernui/test/TestViewPager.kt
@@ -19,7 +19,10 @@
 package icyllis.modernui.test
 
 import icyllis.modernui.ModernUI
+import icyllis.modernui.R
 import icyllis.modernui.fragment.Fragment
+import icyllis.modernui.graphics.drawable.ColorDrawable
+import icyllis.modernui.resources.TypedValue
 import icyllis.modernui.util.DataSet
 import icyllis.modernui.util.Log
 import icyllis.modernui.view.Gravity
@@ -27,6 +30,8 @@ import icyllis.modernui.view.LayoutInflater
 import icyllis.modernui.view.View
 import icyllis.modernui.view.View.OnFocusChangeListener
 import icyllis.modernui.view.ViewGroup
+import icyllis.modernui.view.ViewGroup.LayoutParams.MATCH_PARENT
+import icyllis.modernui.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import icyllis.modernui.widget.*
 
 fun main() {
@@ -36,62 +41,124 @@ fun main() {
 }
 
 class TestViewPager : Fragment() {
+    private lateinit var pager: ViewPager
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: DataSet?
     ): View {
-        val pager = ViewPager(context)
-        pager.adapter = MyAdapter()
-        pager.isFocusableInTouchMode = true
-        pager.isKeyboardNavigationCluster = true
-
-        // press 'tab' key to take focus, and use arrow keys
-        pager.onFocusChangeListener = OnFocusChangeListener { v, hasFocus ->
-            Log.info(
-                null,
-                "{} focus change: {}",
-                v,
-                hasFocus
-            )
+        return LinearLayout(context).apply {
+            orientation = LinearLayout.VERTICAL
+            gravity = Gravity.CENTER
+            addView(ViewPager(context).apply pager@{
+                pager = this
+                adapter = MyAdapter()
+                isFocusableInTouchMode = true
+                isKeyboardNavigationCluster = true
+                // press 'tab' key to take focus, and use arrow keys
+                onFocusChangeListener = OnFocusChangeListener { v, hasFocus ->
+                    Log.info(null, "{} focus change: {}", v, hasFocus)
+                }
+                addView(LinearPagerIndicator(context).apply {
+                    setPager(this@pager)
+                }, ViewPager.LayoutParams().apply {
+                    height = dp(30F)
+                    isDecor = true
+                    gravity = Gravity.CENTER_HORIZONTAL
+                })
+                setPadding(dp(8F), 0, dp(8F), 0)
+                pageMargin = dp(16F)
+            }, LinearLayout.LayoutParams(dp(480F), dp(360F)).apply {
+                gravity = Gravity.CENTER
+            })
+            addView(Button(context, null, null, R.style.Widget_Material3_Button_OutlinedButton).apply {
+                text = "Increment count"
+                setOnClickListener {
+                    (pager.adapter as MyAdapter).apply {
+                        val curItem = pager.currentItem
+                        count++
+                        notifyDataSetChanged()
+                        val nextItem = curItem + 1
+                        if (nextItem < count) {
+                            Log.info(null, "Set item to {}", nextItem)
+                            pager.currentItem = nextItem
+                        }
+                    }
+                }
+            }, LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT).apply {
+                gravity = Gravity.CENTER
+                setMargins(0, dp(4F), 0, dp(4F))
+            })
+            addView(Button(context, null, null, R.style.Widget_Material3_Button_OutlinedButton).apply {
+                text = "Decrement count"
+                setOnClickListener {
+                    (pager.adapter as MyAdapter).apply {
+                        count--
+                        notifyDataSetChanged()
+                    }
+                }
+            }, LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT).apply {
+                gravity = Gravity.CENTER
+                setMargins(0, dp(4F), 0, dp(4F))
+            })
+            addView(ToggleButton(context, null, null, R.style.Widget_Material3_Button_OutlinedButton).apply {
+                text = "Toggle LTR/RTL"
+                setOnCheckedChangeListener { _, checked ->
+                    (pager.parent as? View)?.apply {
+                        layoutDirection = if (checked) View.LAYOUT_DIRECTION_RTL else View.LAYOUT_DIRECTION_LTR
+                    }
+                }
+            }, LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT).apply {
+                gravity = Gravity.CENTER
+                setMargins(0, dp(4F), 0, dp(4F))
+            })
+            layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
         }
-        run {
-            val indicator = LinearPagerIndicator(context)
-            indicator.setPager(pager)
-            val lp = ViewPager.LayoutParams()
-            lp.height = pager.dp(30F)
-            lp.isDecor = true
-            lp.gravity = Gravity.CENTER_HORIZONTAL
-            pager.addView(indicator, lp)
-        }
-        val lp = FrameLayout.LayoutParams(pager.dp(480F), pager.dp(360F))
-        lp.gravity = Gravity.CENTER
-        pager.layoutParams = lp
-
-        // used when pages change too fast
-        //pager.setOffscreenPageLimit(2);
-        return pager
     }
 
     internal class MyAdapter : PagerAdapter() {
+        @JvmField
+        var count: Int = 7
+
         override fun getCount(): Int {
-            return 7
+            return count
         }
 
         override fun instantiateItem(container: ViewGroup, position: Int): Any {
-            val tv = TextView(container.context)
-            tv.text = "This is page ${position + 1}"
-            tv.gravity = Gravity.CENTER
-            container.addView(tv)
-            return tv
+            Log.info(null, "Instantiate item {}", position)
+            return TextView(container.context).apply {
+                text = position.toString()
+                gravity = Gravity.CENTER
+                val value = TypedValue()
+                context.theme.resolveAttribute(R.ns, R.attr.colorSurface, value, true)
+                background = ColorDrawable(value.data)
+            }.also {
+                container.addView(it, 0)
+            }
         }
 
         override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+            Log.info(null, "Destroy item {}", position)
             container.removeView(`object` as View)
         }
 
         override fun isViewFromObject(view: View, `object`: Any): Boolean {
             return view === `object`
         }
+
+        override fun getItemPosition(`object`: Any): Int {
+            val tv = `object` as TextView
+            val pos = tv.text.toString().toInt()
+            if (pos < count)
+                return pos
+            return POSITION_NONE
+        }
+
+        /*override fun getPageWidth(position: Int): Float {
+            return if (position and 1 == 0)
+                1.0F
+            else 0.5F
+        }*/
     }
 }
